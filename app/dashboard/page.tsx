ca8dbb3bf9b459a54f6192d8ed9b971c4a935c81
Andersen Cypriano <andersen.cypriano@gmail.com>
2025-12-11 13:56:53 -0300

    feat: add social login

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import { useSession, signOut } from "@/app/lib/auth/auth-client";
 import { useEffect } from "react";
+import Image from "next/image";
 
 export default function DashboardPage() {
     const router = useRouter();
@@ -20,12 +21,17 @@ export default function DashboardPage() {
         return <p className="text-center mt-8 text-white">Redirecting...</p>;
 
     const { user } = session;
+    console.log(user);
 
     return (
         <main className="max-w-md h-screen flex items-center justify-center flex-col mx-auto p-6 space-y-4 text-white">
             <h1 className="text-2xl font-bold">Dashboard</h1>
             <p>Welcome, {user.name || "User"}!</p>
             <p>Email: {user.email}</p>
+            {user.image && (
+                <Image src={user.image} alt={user.name || "User"} width={100} height={100} className="rounded-full" />
+            )}
+
             <button
                 onClick={() => signOut()}
                 className="w-full bg-white text-black font-medium rounded-md px-4 py-2 hover:bg-gray-200"
--- app/lib/auth/auth.ts
+++ app/lib/auth/auth.ts
@@ -3,7 +3,20 @@ import { prismaAdapter } from 'better-auth/adapters/prisma'
 import prisma from '../prisma'
 
 export const auth = betterAuth({
-   emailAndPassword: { 
+  socialProviders: {
+    google: { 
+      clientId: process.env.GOOGLE_CLIENT_ID as string, 
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+      mapProfileToUser: (profile: any) => {
+        return {
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        }
+      }
+    }, 
+  },
+  emailAndPassword: { 
     enabled: true, 
   }, 
   database: prismaAdapter(prisma, {
